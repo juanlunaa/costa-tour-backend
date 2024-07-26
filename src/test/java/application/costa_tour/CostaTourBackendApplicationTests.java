7f8715ba6b719277b76743466edf13d1a6a1ceb4
juanlunaa <jp3165239@gmail.com>
2024-07-26 12:35:25 -0500

    first commit

---
+++ src/test/java/application/costa_tour/CostaTourBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package application.costa_tour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CostaTourBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
