00b816b4265959b0eb85fb0c71463685bdaa01bb
juanlunaa <jp3165239@gmail.com>
2024-07-27 17:50:38 -0500

    feat: Add client register and update avatar of client

--- src/main/java/application/costa_tour/controller/ClienteController.java
+++ src/main/java/application/costa_tour/controller/ClienteController.java
@@ -1,13 +1,13 @@
 package application.costa_tour.controller;
 
+import application.costa_tour.dto.ClienteCreateDTO;
+import application.costa_tour.dto.ClienteDTO;
 import application.costa_tour.model.Cliente;
 import application.costa_tour.service.ClienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/cliente")
@@ -18,7 +18,22 @@ public class ClienteController {
 
     @GetMapping
     public ResponseEntity<?> clientByDni (@RequestParam String dni) {
-        Cliente cliente = clienteService.getClientByDni(dni);
+        ClienteDTO cliente = clienteService.getClientByDni(dni);
         return ResponseEntity.ok(cliente);
+    }
+
+    @PostMapping("/create")
+    public ResponseEntity<?> createClient (@ModelAttribute ClienteCreateDTO cliente) {
+        clienteService.createClient(cliente);
+        return ResponseEntity.ok("Cliente creado correctamente");
+    }
+
+    @PostMapping("/update/avatar")
+    public ResponseEntity<?> changeAvatar (
+            @RequestParam("dni") String dniCliente,
+            @RequestParam("avatar") MultipartFile file
+    ) {
+        clienteService.uploadAvatar(dniCliente, file);
+        return ResponseEntity.ok("Imagen actulizada correctamente");
     }
 }
