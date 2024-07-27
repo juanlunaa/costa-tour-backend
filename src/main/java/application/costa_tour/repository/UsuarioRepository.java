cebfcede39ea4bb68acdf0aab2ca8ade66768d5b
juanlunaa <jp3165239@gmail.com>
2024-07-27 17:47:15 -0500

    feat: Add userRepository to save basic user info in db

---
+++ src/main/java/application/costa_tour/repository/UsuarioRepository.java
@@ -0,0 +1,9 @@
+package application.costa_tour.repository;
+
+import application.costa_tour.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+}
