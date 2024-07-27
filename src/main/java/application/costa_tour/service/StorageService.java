abbca6de42166b2549e53ba5cab64177ab7d542b
juanlunaa <jp3165239@gmail.com>
2024-07-27 18:48:53 -0500

    feat: Add loadFile to provide resource stored in the server

--- src/main/java/application/costa_tour/service/StorageService.java
+++ src/main/java/application/costa_tour/service/StorageService.java
@@ -1,10 +1,13 @@
 package application.costa_tour.service;
 
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -28,6 +31,21 @@ public class StorageService {
             }
         } catch (IOException e) {
             throw new RuntimeException("Failed saving file.", e);
+        }
+    }
+
+    public Resource loadFile (String filename) {
+        try {
+            Path file = Paths.get(filename);
+            Resource resource = new UrlResource(file.toUri());
+
+            if (resource.exists() || resource.isReadable()) {
+                return resource;
+            } else {
+                throw new RuntimeException("Couldn't read file.");
+            }
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("Couldn't read file.");
         }
     }
 }
