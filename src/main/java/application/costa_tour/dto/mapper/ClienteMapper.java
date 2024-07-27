40a84adfbc5857a36e27f7c7f8b04ec7e8ff3e7f
juanlunaa <jp3165239@gmail.com>
2024-07-27 17:44:26 -0500

    feat: Add client mappers from Entity to DTO and DTO to Entity

---
+++ src/main/java/application/costa_tour/dto/mapper/ClienteMapper.java
@@ -0,0 +1,29 @@
+package application.costa_tour.dto.mapper;
+
+import application.costa_tour.dto.ClienteDTO;
+import application.costa_tour.model.Cliente;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface ClienteMapper {
+
+    ClienteMapper mapper = Mappers.getMapper(ClienteMapper.class);
+
+    @Mapping(source = "usuario.tipo", target = "tipoUsuario")
+    @Mapping(source = "usuario.nombre", target = "nombre")
+    @Mapping(source = "usuario.apellido", target = "apellido")
+    @Mapping(source = "usuario.email", target = "email")
+    @Mapping(source = "usuario.telefono", target = "telefono")
+    @Mapping(source = "usuario.imagenPerfil", target = "avatar")
+    @Mapping(source = "tipoDocumento.descripcion", target = "tipoDocumento")
+    @Mapping(source = "ciudad.id", target = "ciudad.id")
+    @Mapping(source = "ciudad.nombre", target = "ciudad.name")
+    @Mapping(source = "ciudad.estado.id", target = "estado.id")
+    @Mapping(source = "ciudad.estado.nombre", target = "estado.name")
+    @Mapping(source = "ciudad.estado.pais.id", target = "pais.id")
+    @Mapping(source = "ciudad.estado.pais.nombre", target = "pais.name")
+    ClienteDTO clienteToClienteDto (Cliente cliente);
+
+}
