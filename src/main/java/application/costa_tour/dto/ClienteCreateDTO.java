c1b864383fc57ad61a36e2a7e6a7ad201be64cad
juanlunaa <jp3165239@gmail.com>
2024-07-28 16:08:53 -0500

    refactor: Add spring validation notations

--- src/main/java/application/costa_tour/dto/ClienteCreateDTO.java
+++ src/main/java/application/costa_tour/dto/ClienteCreateDTO.java
@@ -1,5 +1,8 @@
 package application.costa_tour.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -7,16 +10,24 @@ import java.util.Date;
 
 @Data
 public class ClienteCreateDTO {
+    @NotEmpty
     private String dni;
+    @NotNull
     private Long idTipoDocumento;
-    private String tipoUsuario;
+    @NotEmpty
     private String nombre;
+    @NotEmpty
     private String apellido;
-
+    @NotNull
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date fechaNacimiento;
+    @NotEmpty
+    @Email
     private String email;
+    @NotEmpty
     private String password;
+    @NotEmpty
     private String telefono;
+    @NotNull
     private Long idCiudad;
 }
