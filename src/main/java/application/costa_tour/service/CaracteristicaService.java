56a8e435f776217cf12b3aa7cd7bca2650615d1f
juanlunaa <jp3165239@gmail.com>
2024-09-20 18:51:31 -0500

    add method to validate the existence of a list of Caracteristica

--- src/main/java/application/costa_tour/service/CaracteristicaService.java
+++ src/main/java/application/costa_tour/service/CaracteristicaService.java
@@ -1,9 +1,14 @@
 package application.costa_tour.service;
 
+import application.costa_tour.exception.ResourceNotFoundException;
 import application.costa_tour.model.Caracteristica;
 import application.costa_tour.repository.CaracteristicaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class CaracteristicaService {
@@ -13,6 +18,18 @@ public class CaracteristicaService {
 
     public Caracteristica getCaracteristica (Long id) {
         return caracteristicaRepository.findById(id).orElse(null);
+    }
+
+    public void isExistsCaracteristicas (List<Long> idCaracteristicasList) {
+        Map<Long, Caracteristica> caracteristicasMap = caracteristicaRepository.findAll().stream()
+                .collect(Collectors.toMap(Caracteristica::getId, c -> c));
+
+        for (Long id : idCaracteristicasList) {
+            if (!caracteristicasMap.containsKey(id)) {
+                throw new ResourceNotFoundException(String
+                        .format("Caracteristica not found for id=%s", id));
+            }
+        }
     }
 
 }
