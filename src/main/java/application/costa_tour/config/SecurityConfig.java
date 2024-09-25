071ccff28caf63bc2f55a21f17ef57aa49b1e10b
juanlunaa <jp3165239@gmail.com>
2024-09-25 15:33:01 -0500

    add basic configuration to spring security (public routes)

---
+++ src/main/java/application/costa_tour/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package application.costa_tour.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(req ->
+                    req
+                        .requestMatchers(
+                                "/user/auth",
+                                "/plan/all",
+                                "/plan/{id}",
+                                "/files/**",
+                                "/location/**"
+                        ).permitAll()
+                        .anyRequest().authenticated()
+                    )
+                .formLogin(Customizer.withDefaults())
+                .build();
+    }
+
+}
