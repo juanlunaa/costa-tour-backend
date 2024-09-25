f15900e6990fbd2a0c81ad7a9e52238a6552b931
juanlunaa <jp3165239@gmail.com>
2024-09-25 18:28:13 -0500

    add JWT authentication filter, authentication provider and edit routes

--- src/main/java/application/costa_tour/config/SecurityConfig.java
+++ src/main/java/application/costa_tour/config/SecurityConfig.java
@@ -1,15 +1,28 @@
 package application.costa_tour.config;
 
+import application.costa_tour.jwt.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+//  Se inyecta el filtro personalizado de JWT para añadirlo a la cadena de filtros
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthFilter;
+
+//  Se inyecta el AuthenticationProvider configurado en ApplicationConfig
+    @Autowired
+    private AuthenticationProvider authProvider;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -24,9 +37,16 @@ public class SecurityConfig {
                                 "/files/**",
                                 "/location/**"
                         ).permitAll()
+                        .requestMatchers("/plan/**").authenticated()
                         .anyRequest().authenticated()
                     )
-                .formLogin(Customizer.withDefaults())
+//              No se crearan ni se usaran sesiones HTTP para almacenar información sobre el usuario autenticado
+                .sessionManagement(sessionManager ->
+                        sessionManager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+//              Setteo del AuthenticationProvider
+                .authenticationProvider(authProvider)
+//              Se añade el filtro de JWT a la cadena de filtros
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
                 .build();
     }
 
