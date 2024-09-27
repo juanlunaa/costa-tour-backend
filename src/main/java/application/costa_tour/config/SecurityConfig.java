d8f04d651555f7888ef53e8f325d2a4945269a98
juanlunaa <jp3165239@gmail.com>
2024-09-26 23:21:46 -0500

    add other routes of /user that require authentication

--- src/main/java/application/costa_tour/config/SecurityConfig.java
+++ src/main/java/application/costa_tour/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -38,6 +37,7 @@ public class SecurityConfig {
                                 "/location/**"
                         ).permitAll()
                         .requestMatchers("/plan/**").authenticated()
+                        .requestMatchers("/user/**").authenticated()
                         .anyRequest().authenticated()
                     )
 //              No se crearan ni se usaran sesiones HTTP para almacenar información sobre el usuario autenticado
